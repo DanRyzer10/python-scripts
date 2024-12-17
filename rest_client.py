233a08e03ee92f87f941502e25061fb6615149ff
Angel Zambrano <angeldaniel6709@gmail.com>
2024-12-16 22:00:05 -0500

    terminado script de automatizacion

--- rest_client.py
+++ rest_client.py
@@ -2,34 +2,58 @@
 import requests
 import json
 import os
+import base64
 
 
-token:str = "7fd157231fe0054b477fba069f8d16b39a926298536aab4113b8c229ac59d125bfe2ce78e4da2eae3d52ee740e93b92fea94a5ab81fbe4fc2683aa71d3f7c528c1fb1314c477d82bde0c02373edc5b33b3d4260bdd01c5b2e063e2654d796225f2fb14ce79578404dbdc98543773864f8ac68d3ef962ed266c16ea276e04a85a"
+token:str = "474371b370470a349d4b49b2fa6032f1a75ebe99f5ce59376e5f53ba2ff5fe6d3b495b4777f777f1a1fb3fc32deb52f73f394abd0299bb578d8b48f6ff6e14c08cda5a2e62e2d44f7f0d02b7e83c7256380247f2bf3df8edefc406728997d49073f9e94766d18640238bcde29edddb60bd3f8587126cc16db839f8b13879b763"
 
 headers  = {
     "Authorization": f"Bearer {token}",
     "Content-Type": "application/json"
 }
 total_countries:list = []
-
+base_url = "https://h45w16tt-1337.use2.devtunnels.ms"
 for i in range(1,4):
     print(i)
-    url:str = f"https://strapi.iss.com.ec/api/countries?pagination[page]={i}&pagination[pageSize]=100&fields[0]=name&fields[1]=code&fields[2]=number"
+    url:str = f"{base_url}/api/countries?pagination[page]={i}&pagination[pageSize]=100"
     request = requests.get(url,headers=headers)
-    response = request.json()
-    countries = response["data"]
-    total_countries.extend(countries)
+    if request.status_code == 200:
+        try:
+            response = request.json()
+            print(response)
+            countries = response["data"]
+            total_countries.extend(countries)
+        except ValueError as e:
+            print(f"Error decoding JSON: {e}")
+            print(f"Response content: {request.text}")
+    else:
+        print(f"Request failed with status code: {request.status_code}")
     
 print(len(total_countries))
-out_folder = "./out"
+out_folder = "out"
+
 for country in total_countries:
-    curr = country["attributes"]["code"] + ".svg"
-    #buscar en una ruta out si existe ese archivo
-    if curr in os.listdir(out_folder):
-        print(f"El archivo {curr} ya existe")
-        # si existe leer su contenido 
-        with open(os.path.join(out_folder,curr)) as file:
-            print(file.read())
+    
+    curr_id = country["id"]
+    curr_file = country["attributes"]["code"] + ".svg"
+    curr_url = f"{base_url}/api/countries/{curr_id}"
+    if curr_file in os.listdir(out_folder):
+        with open(f"{out_folder}/{curr_file}", "r", encoding="utf-8") as file:
+            svg_content:str = file.read()
+            # svg_b64 = base64.b64encode(svg_content.encode("utf-8")).decode("utf-8")
+            country["attributes"]["icon"] = svg_content
+         
+            print(f"Updating {country}")
+            print("------------------------------------------")
+            
+            response = requests.put(curr_url,headers=headers,json={"data":country["attributes"]})
+            print(response)
+            
+            if response.status_code == 200:
+                print(f"Successfully updated country {curr_id}")
+            else:
+                print(f"Failed to update country {curr_id} with status code: {response.status_code}")
+                print(f"Response content: {response.text}")
         continue
     
     
